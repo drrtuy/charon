00424d0c92071a334ce831266af9a4810d11595f
root <drrtuy@gmail.com>
2015-10-17 16:00:14 +0200

    Сб окт 17 16:00:14 EET 2015

--- charon/views.py
+++ charon/views.py
@@ -1,9 +1,12 @@
 from charon import app
 
-@app.route("/")
-@app.route("/index")
-def hello():
-    return "Hello World!"
+@app.route("api.charon.shopster.ru/v1/radius/subs/")
+def doAllowUser():
+    return str(method) + str(request.headers) + str(values)
+
+@app.route("charon.shopster.ru/postauth/")
+def doPostauth():
+    return str(method) + str(request.headers) + str(values)
 
 if __name__ == "__main__":
     app.run()
