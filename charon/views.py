a628b98250a6ded65cbba708caecc42020e718e8
root <drrtuy@gmail.com>
2015-10-17 17:10:43 +0200

    Сб окт 17 17:10:43 EET 2015

--- charon/views.py
+++ charon/views.py
@@ -1,12 +1,13 @@
 from charon import app
+from flask import request
 
 @app.route("/v1/radius/subs/")
 def doAllowUser():
-    return str(request.method) + str(request.headers) + str(request.values)
+    return str(flask.request.method) + str(flask.request.headers) + str(flask.request.values)
 
 @app.route("/postauth/")
 def doPostauth():
-    return str(request.method) + str(request.headers) + str(request.values)
+    return str(flask.request.method) + str(flask.request.headers) + str(flask.request.values)
 
 if __name__ == "__main__":
     app.run()
