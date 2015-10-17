f45a550ee33edaa0d833c593004e3ff0a69eea1e
root <drrtuy@gmail.com>
2015-10-17 20:33:00 +0200

    Сб окт 17 20:33:00 EET 2015

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM flask-image
 MAINTAINER drrtuy
 EXPOSE 8080
-#COPY app requirements.txt server.py /var/www/
-RUN git clone https://github.com/drrtuy/charon /var/www/
-ENTRYPOINT ["/usr/bin/python","/var/www/server.py"]
+#RUN git clone https://github.com/drrtuy/charon /var/www/
+#ENTRYPOINT ["/usr/bin/python","/var/www/server.py"]
 
--- charon/views.py
+++ charon/views.py
@@ -3,11 +3,11 @@ from flask import request
 
 @app.route("/v1/radius/subs/")
 def doAllowUser():
-    return str(flask.request.method) + str(flask.request.headers) + str(flask.request.values)
+    return str(request.method) + str(request.headers) + str(request.values)
 
 @app.route("/postauth/")
 def doPostauth():
-    return str(flask.request.method) + str(flask.request.headers) + str(flask.request.values)
+    return str(request.method) + str(request.headers) + str(request.values)
 
 if __name__ == "__main__":
     app.run()
