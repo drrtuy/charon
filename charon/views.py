91264294b89d6740fbef898e13bf288abebf8b10
root <drrtuy@gmail.com>
2015-10-17 16:24:06 +0200

    Сб окт 17 16:24:06 EET 2015

--- charon/views.py
+++ charon/views.py
@@ -2,11 +2,11 @@ from charon import app
 
 @app.route("/v1/radius/subs/")
 def doAllowUser():
-    return str(method) + str(request.headers) + str(values)
+    return str(request.method) + str(request.headers) + str(request.values)
 
 @app.route("/postauth/")
 def doPostauth():
-    return str(method) + str(request.headers) + str(values)
+    return str(request.method) + str(request.headers) + str(request.values)
 
 if __name__ == "__main__":
     app.run()
