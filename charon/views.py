09ab5117ac905d670b29426e47e74d90477129d8
drrtuy <drrtuy@gmail.com>
2015-10-18 15:28:35 +0000

    Sun Oct 18 15:26:17 UTC 2015

--- charon/views.py
+++ charon/views.py
@@ -1,5 +1,5 @@
- from charon import app
-from flask import request, render_template
+from charon import app
+from flask import request, render_template, abort
 
 @app.route("/v1/radius/subs/", methods=['POST','GET'])
 def doAllowUser():
@@ -8,11 +8,33 @@ def doAllowUser():
         r += str(i)
         r += "\n"
     print request.values
-    return render_template('subs.html')
+    extradata = {'linkorig': 'www.ya.ru',\
+        
+    }
+    return render_template('subs.html', extradata=extradata)
 
-@app.route("/postauth/")
-def doPostauth():
-    return str(request.method) + str(request.headers) + str(request.values)
+def doCheckPostpostauthVals(request):
+    return True
+
+@app.route("/postauth/", methods=['POST','GET'])
+def doPostpostauth():
+    if request.method == 'POST' and doCheckPostpostauthVals(request):
+        print request.values
+        extradata = {'mac': request.values.get('mac', 'empty'),\
+            'ip': request.values.get('ip', 'empty'),\
+            'username': request.values.get('username', 'empty'),\
+            'linkorig': request.values.get('link-orig', 'empty'),\
+            'linklogin': request.values.get('link-login', 'empty'),\
+            'error': request.values.get('error', 'empty')
+        }
+        return render_template('postpostauth.html', extradata=extradata)
+    elif request.method == 'GET':
+        print request.values
+        extradata = {'linkorig': 'www.ya.ru',\
+        }
+        return render_template('subs.html', extradata=extradata)
+    else:
+        abort(501)
 
 if __name__ == "__main__":
     app.run()
